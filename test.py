fe03c90fe55033bd2b29544abdc663eda30f8ea2
Adam Forsythe-Cheasley <adam@delib.net>
2021-02-18 16:42:18 +0000

    Styling changes for test chart and remove some dependency on pptx

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 from docx import Document
+from docx.shared import Pt, Cm
 from pptx.chart.data import CategoryChartData
-from pptx.util import Pt, Inches
 from pptx.enum.chart import XL_CHART_TYPE
 
 
@@ -20,7 +20,7 @@ p = document.add_paragraph('An example chart:')
 chart_data = CategoryChartData()
 chart_data.categories = ['East', 'West', 'Midwest']
 chart_data.add_series('Series 1', (19.2, 21.4, 16.7))
-x, y, cx, cy = Inches(2), Inches(2), Inches(6), Inches(4.5)
+x, y, cx, cy = Cm(2), Cm(2), Cm(15), Cm(10)
 chart = document.add_chart(XL_CHART_TYPE.BAR_CLUSTERED, x, y, cx, cy, chart_data)
 
 chart.chart_style = 3  # Makes the chart monochrome
@@ -31,8 +31,10 @@ text_frame.text = 'Chart Title Heres'
 paragraphs = text_frame.paragraphs
 paragraph = paragraphs[0]
 paragraph.font.size = Pt(18)
+chart.value_axis.has_major_gridlines = False
+chart.value_axis.tick_labels.font.size = Pt(9)
 category_axis = chart.category_axis
-category_axis.tick_labels.font.size = Pt(14)
+category_axis.tick_labels.font.size = Pt(11)
 
 document.save(DEMO_FILENAME)
 print 'Done'
